6f66b27e968b7b4ab07d4213c05c64baee7191ac
neville <kilonzoneville@gmail.com>
2018-08-30 13:03:36 +0300

    add the javascript code for the side nav

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,15 @@ var morphing = anime({
       var el = document.querySelector('#JSobject pre');
       el.innerHTML = JSON.stringify(obj);
     }
-  });  
+  });  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
