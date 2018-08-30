192dc2ba83c458c35ba15d283ea607d284913535
neville <kilonzoneville@gmail.com>
2018-08-30 13:08:48 +0300

    add the javascript code for the rotating

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,30 @@ var morphing = anime({
       var el = document.querySelector('#JSobject pre');
       el.innerHTML = JSON.stringify(obj);
     }
-  });  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+  });  
+
+
+var CSStransforms = anime({
+    targets: '#CSStransforms .el',
+    translateX: 50,
+    translateY: 50,
+    scale: 1,
+    rotate: '2turn'
+  });
+
+var cssProperties = anime({
+    targets: '#cssProperties #svgAttributes',
+    opacity: .5,
+    left: '240px',
+    backgroundColor: [
+        {value: 'black'}, // Or #FFFFFF
+        // {value: 'rgb(255, 0, 0)'},
+        // {value: 'hsl(100, 60%, 60%)'}
+      ],
+    borderRadius: ['0em', '2em'],
+    easing: 'easeInOutQuad'
+});
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
@@ -34,3 +57,41 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+/* styling the name plate */
+
+
+
+// $(document).ready(function() {
+//     $(".title").lettering();
+//     $(".buttons").lettering();
+//   });
+  
+  
+  
+  
+  
+//   $(document).ready(function() {
+//     animation();
+//   }, 1000);
+  
+//   $('.buttons').click(function() {
+//     animation();
+//   });
+  
+  
+//   function animation() {
+//     var title1 = new TimelineMax();
+//     title1.to(".buttons", 0, {visibility: 'hidden', opacity: 0})
+//     title1.staggerFromTo(".title span", 0.5, 
+//     {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
+//     {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
+//     title1.to(".buttons", 0.2, {visibility: 'visible' ,opacity: 1})
+//   }
+
+
+// var svgAttributes = anime({
+//     targets: '#svgAttributes polygon',
+//     points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+//     easing: 'easeInOutExpo'
+//   });
